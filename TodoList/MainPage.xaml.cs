bde22d88565911fb239fced4b8c12bf99cd209d1
nurayklc <nuraykilic0101@gmail.com>
2022-06-10 15:49:32 +0300

    Complete To Do List

---
+++ TodoList/MainPage.xaml.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace TodoList
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+        //public void HandleTextChanged(object sender, TextChangedEventArgs args)
+        //{
+        //    Console.WriteLine(args.NewTextValue);
+        //}
+
+        //public void HandleEnterPress(object sender, EventArgs args)
+        //{
+        //    Console.WriteLine("Enter Pressed");
+        //    Label newTodo = new Label();
+        //    newTodo.FontSize = 20;
+        //    newTodo.Text = InputField.Text;
+        //    TodoList.Children.Add(newTodo);
+        //    InputField.Text = string.Empty;
+        //}
+    }
+}
---
+++ TodoList/ViewModels/TodoListViewModel.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Text;
+using System.Windows.Input;
+using TodoList.Models;
+using Xamarin.Forms;
+
+namespace TodoList.ViewModels
+{
+    class TodoListViewModel
+    {
+        public ObservableCollection<TodoItem> TodoItems { get; set; }
+        public TodoListViewModel()
+        {
+            TodoItems = new ObservableCollection<TodoItem>();
+
+            TodoItems.Add(new TodoItem("Todo 1", true));
+        }
+        public string NewTodoInputValue { get; set; }
+        public ICommand AddTodoCommand => new Command(AddTodoItem);
+
+        void AddTodoItem()
+        {
+            TodoItems.Add(new TodoItem(NewTodoInputValue, false));
+            NewTodoInputValue = string.Empty;
+        }
+        public ICommand RemoveTodoCommand => new Command(RemoveTodoItem);
+
+        void RemoveTodoItem(object o)
+        {
+            TodoItem todoItemBeingRemoved = o as TodoItem;
+            TodoItems.Remove(todoItemBeingRemoved);
+        }
+    }
+}
